15317dac8f473ac5ec069f24b342f3ba4dea56ab
Rareș <72668860+Rares-AndreiVasiu@users.noreply.github.com>
2022-05-01 14:38:02 +0300

    Create Eficienta 30.cpp

---
+++ Rezolvari/Eficienta 30.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+ifstream fin("NUMERE.IN");
+
+int n, x, y, a, b;
+
+int main()
+{
+	fin >> n;
+
+	fin >> x;
+
+	a = x;
+
+	while (fin >> y)
+	{
+		if ((int)y - int(x) == 0)
+		{
+
+		}
+	}
+}
