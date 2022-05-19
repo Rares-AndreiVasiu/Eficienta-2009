edea1eccc98b24406fc3f624188ecec060869212
Rareș Vasiu <72668860+Rares-AndreiVasiu@users.noreply.github.com>
2022-05-19 11:45:54 +0300

    Update Eficienta 99.cpp

--- Rezolvari/Eficienta 99.cpp
+++ Rezolvari/Eficienta 99.cpp
@@ -7,30 +7,38 @@ int v[10001], st, dr, l = 1, lmax, i;
 
 int main()
 {
-	fin >> v[++i];
+    fin >> v[++i];
 
-	while (fin >> v[++i])
-	{
-		if (v[i - 1] % 2 != v[i] % 2)
-		{
-			l++;
-		}
-		else
-		{
-			if (l > lmax)
-			{
-				lmax = l;
+    while (fin >> v[++i])
+    {
+        if (v[i - 1] % 2 != v[i] % 2)
+        {
+            l++;
+        }
+        else
+        {
+            if (l > lmax)
+            {
+                lmax = l;
 
-				dr = i - 1;
-				st = i - l;
-			}
-			l = 1;
-		}
-	}
-	std::cout << lmax << '\n';
+                dr = i - 1;
+                st = i - l;
+            }
+            l = 1;
+        }
+    }
 
-	for (int i = st; i <= dr; ++i)
-		std::cout << v[i] << ' ';
-	
-	return 0;
+    if (l > lmax)
+    {
+        lmax = l;
+
+        dr = i - 1;
+        st = i - l;
+    }
+    std::cout << lmax << '\n';
+
+    for (int i = st; i <= dr; ++i)
+        std::cout << v[i] << ' ';
+
+    return 0;
 }
